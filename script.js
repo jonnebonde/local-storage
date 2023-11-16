96669211b6e65e06962e342669f1976af5e580fb
jonnebonde <jonnebonde@gmail.com>
2023-11-16 18:27:33 +0100

    refactor

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
+const dataInput = document.getElementById("#dataInput");
+const saveButton = document.getElementById("#save");
+const fetchButton = document.getElementById("#load");
+
+
+saveButton.addEventListener("click", saveToLocalStorage);
+fetchButton.addEventListener("click", fetchFromLocalStorage);
+
 function saveToLocalStorage() {
-  const data = document.getElementById('dataInput').value;
-  localStorage.setItem('myData', data);
-  alert('Data saved to local storage!');
+  const data = document.getElementById("dataInput").value;
+  localStorage.setItem("myData", data);
+  alert("Data saved to local storage!");
 }
 
 function fetchFromLocalStorage() {
-  const data = localStorage.getItem('myData');
+  const data = localStorage.getItem("myData");
   if (data) {
-      document.getElementById('output').textContent = `Fetched Data: ${data}`;
+    document.getElementById("output").textContent = `Fetched Data: ${data}`;
   } else {
-      alert('No data found in local storage.');
+    alert("No data found in local storage.");
   }
 }
 
